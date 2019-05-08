62ce920b09136b9f52659d3096573b36f02a3619
Поляков Андрей Владимирович <avpgenium@yandex.ru>
2019-05-09 02:17:33 +0500

    initial commit

---
+++ PetSearchIosApp/PetSearchIosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PetSearchIosApp
+//
+//  Created by Andrei Poliakov on 09.05.2019.
+//  Copyright © 2019 Andrei Poliakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
